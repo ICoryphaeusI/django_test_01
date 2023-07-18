c5ccb9cd3f568023af5b05f4cb3fdb9efe7d7f4d
ICoryphaeusI <corryphaeus@gmail.com>
2023-07-18 18:08:25 +0300

    loading csv files with overwriting old ones

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import os
 
 # Create your models here.
+
+class File(models.Model):
+    file = models.FileField(upload_to='files')
+
+    def __str__(self):
+        return os.path.basename(self.file.name)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import File
+import pandas as pd
+import chardet
+import io
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 # Create your views here.
+
+def create_db(file_data):
+    result = chardet.detect(file_data)
+    encoding = result['encoding']
+
+    file_data = file_data.decode(encoding)
+    df = pd.read_csv(io.StringIO(file_data), delimiter=',')
+    return df
+
+def index(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        file_name = file.name
+        file_path = 'files/' + file_name
+
+        # Удаление существующего файла, если он есть
+        if default_storage.exists(file_path):
+            default_storage.delete(file_path)
+
+        # Сохранение нового файла
+        default_storage.save(file_path, ContentFile(file.read()))
+
+        # Чтение файла и вызов функции create_db()
+        with default_storage.open(file_path) as file:
+            create_db(file.read())
+
+    return render(request, 'api/index.html')
